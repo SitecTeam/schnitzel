dc90bf4a75d3f09b1d64f78b842fd77fbab6646f
AndrejJurisic <124591321+AndrejJurisic@users.noreply.github.com>
2026-02-19 10:59:39 +0100

    SCH-82 / Add custom colors (#1)
    * install shadcn button and create button variants
    
    * add custom colors
    
    * update button
    
    * create a reusable button for podcast links
    
    * refactor button component for improved styling and layout
    
    * fix formating problem
    
    * add rel="noopener noreferrer"
    
    * refactor podcast button component to use anchor tag and improve styling

--- apps/cms/next.config.mjs
+++ apps/cms/next.config.mjs
@@ -1,20 +1,20 @@
-import { withPayload } from '@payloadcms/next/withPayload'
+import { withPayload } from "@payloadcms/next/withPayload";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Packages with Cloudflare Workers (workerd) specific code
   // Read more: https://opennext.js.org/cloudflare/howtos/workerd
-  serverExternalPackages: ['jose', 'pg-cloudflare'],
+  serverExternalPackages: ["jose", "pg-cloudflare"],
 
-  webpack: (webpackConfig) => {
+  webpack: webpackConfig => {
     webpackConfig.resolve.extensionAlias = {
-      '.cjs': ['.cts', '.cjs'],
-      '.js': ['.ts', '.tsx', '.js', '.jsx'],
-      '.mjs': ['.mts', '.mjs'],
-    }
+      ".cjs": [".cts", ".cjs"],
+      ".js": [".ts", ".tsx", ".js", ".jsx"],
+      ".mjs": [".mts", ".mjs"],
+    };
 
-    return webpackConfig
+    return webpackConfig;
   },
-}
+};
 
-export default withPayload(nextConfig, { devBundleServerPackages: false })
+export default withPayload(nextConfig, { devBundleServerPackages: false });

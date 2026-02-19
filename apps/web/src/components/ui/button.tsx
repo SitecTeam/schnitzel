01516aa05a589c274fd6132392d66aa127553282
Andrej Jurisic <andrejjurisic99@gmail.com>
2026-02-19 10:22:20 +0100

    update button

--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -14,14 +14,14 @@ const buttonVariants = cva(
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
-        primary: "bg-primary text-primary-foreground",
-        secondary: "bg-transparent text-secondary",
+        primary: "bg-primary text-primary-foreground border-2 border-primary",
+        secondary: "bg-transparent text-secondary border-2 border-secondary",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-fit min-w-0 shrink-0 p-0 rounded-xs",
+        default: "min-w-0 shrink-0 rounded-xs xl:p-4 xl:pl-5 p-2 pl-4",
         xs: "h-6 gap-1 rounded-md px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",

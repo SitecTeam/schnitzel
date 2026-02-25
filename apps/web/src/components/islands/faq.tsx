f282b050434917d51599ebf9bb530c4385101348
Andrej Jurisic <andrejjurisic99@gmail.com>
2026-02-25 10:36:36 +0100

    refactor faq component

--- apps/web/src/components/islands/faq.tsx
+++ apps/web/src/components/islands/faq.tsx
@@ -36,42 +36,49 @@ const faqItems = [
 
 const Faq = () => {
   return (
-    <section className="z-20 mt-1 w-full max-w-376.5 bg-secondary p-4 sm:p-8 lg:-mt-10 lg:p-12 xl:-mt-10 xl:p-16 2xl:-mt-24">
-      <div className="flex flex-col gap-8 lg:flex-row lg:gap-10">
-        <div className="flex-1 text-center text-white sm:text-start">
-          <Typography tag="h1" variant="h1" className="text-white">
-            FAQ
-          </Typography>
-          <Typography
-            variant="body-xl"
-            className="mt-6 text-balance text-primary-foreground lg:min-w-70 xl:max-w-88 xl:min-w-88"
-          >
-            Quick answers to the questions we get the most.
-          </Typography>
-        </div>
+    <section className="flex w-full justify-center">
+      <div className="z-20 mt-1 bg-secondary p-4 sm:p-8 lg:-mt-10 lg:w-[93%] lg:p-9 xl:-mt-10 2xl:-mt-20 2xl:w-[85%] 2xl:p-16">
+        <div className="flex flex-col gap-8 lg:flex-row lg:gap-10">
+          <div className="basis-2/5 text-center text-white sm:text-start xl:basis-[45%]">
+            <Typography tag="h1" variant="h1" className="lg:-mt-2.5">
+              FAQ
+            </Typography>
+            <Typography
+              variant="body-xl"
+              className="mt-6 w-full text-balance text-primary-foreground lg:min-w-70 xl:max-w-88"
+            >
+              Quick answers to the questions we get the most.
+            </Typography>
+          </div>
 
-        <Accordion
-          type="single"
-          collapsible
-          defaultValue="faq-1"
-          className="w-full space-y-4 lg:max-w-178.5 lg:space-y-9 lg:justify-self-end"
-        >
-          {faqItems.map(item => (
-            <AccordionItem key={item.id} value={item.id} className="border-0">
-              <AccordionTrigger className="min-h-17.5 w-full cursor-pointer items-center rounded-none bg-primary-foreground px-4 py-2 text-secondary uppercase hover:no-underline [&>svg]:hidden [&[data-state=open]>div>svg]:rotate-180">
-                <div className="flex w-full items-start justify-between gap-4">
-                  <Typography variant="h5" tag="h5">
-                    {item.question}
+          <Accordion
+            type="single"
+            collapsible
+            defaultValue="faq-1"
+            className="w-full flex-1 space-y-4 lg:space-y-11 lg:justify-self-end"
+          >
+            {faqItems.map(item => (
+              <AccordionItem key={item.id} value={item.id} className="border-0">
+                <AccordionTrigger className="min-h-15.25 w-full cursor-pointer items-center rounded-none bg-primary-foreground px-4 py-2 text-secondary uppercase hover:no-underline [&>svg]:hidden [&[data-state=open]>div>svg]:rotate-180">
+                  <div className="flex w-full items-start justify-between gap-4">
+                    <Typography variant="h5" tag="h5">
+                      {item.question}
+                    </Typography>
+                    <ChevronDown className="size-8 shrink-0 text-secondary transition-transform duration-200 lg:mt-0.5 lg:size-9" />
+                  </div>
+                </AccordionTrigger>
+                <AccordionContent className="pt-4 pb-0 text-primary-foreground">
+                  <Typography
+                    variant="body-lg"
+                    className="leading-tight text-pretty"
+                  >
+                    {item.answer}
                   </Typography>
-                  <ChevronDown className="size-8 shrink-0 text-secondary transition-transform duration-200 lg:mt-0.5 lg:size-9" />
-                </div>
-              </AccordionTrigger>
-              <AccordionContent className="pt-3 pb-0 text-primary-foreground">
-                <Typography variant="body-lg">{item.answer}</Typography>
-              </AccordionContent>
-            </AccordionItem>
-          ))}
-        </Accordion>
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </div>
       </div>
     </section>
   );

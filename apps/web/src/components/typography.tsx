9e43b1349f91113a60bc94da3a2b90fd6b4aa7ce
SitecTeam <sitecteam25@gmail.com>
2026-03-05 11:03:50 +0100

    feat: update typography variants for improved mobile responsiveness (#16)
    * feat: update typography variants for improved mobile responsiveness
    
    * feat: update hero section typography for improved readability on mobile
    
    ---------
    
    Co-authored-by: Djordje Blagojevic <dj.blagojevic@gmail.com>

--- apps/web/src/components/typography.tsx
+++ apps/web/src/components/typography.tsx
@@ -6,22 +6,30 @@ import { cn } from "@/lib/utils";
 export const textVariants = cva("", {
   variants: {
     variant: {
-      "display-lg": "font-anton text-[128px] leading-[140px]",
-      "display-md": "font-anton text-[112px] leading-[124px]",
-      h1: "font-anton text-[96px] leading-[110px]",
-      h2: "font-anton text-[64px] leading-[77px]",
-      h3: "font-anton text-[48px] leading-[58px]",
-      h4: "font-anton text-[40px] leading-[50px]",
-      h5: "font-anton text-[32px] leading-[40px]",
-      h6: "font-anton text-[24px] leading-[31px]",
-      subtitle: "font-anton text-[20px] leading-[28px]",
-      "body-xl": "font-inter text-[24px] leading-[38px]",
-      "body-lg": "font-inter text-[20px] leading-[32px]",
-      "body-md": "font-inter text-[18px] leading-[29px]",
-      "body-sm": "font-inter text-[16px] leading-[27px]",
+      "display-lg":
+        "font-anton text-[72px] leading-[86px]  lg:text-[128px] lg:leading-[140px]",
+      "display-md":
+        "font-anton text-[64px] leading-[77px] lg:text-[112px] lg:leading-[124px]",
+      h1: "font-anton text-[56px] leading-[67px] lg:text-[96px] lg:leading-[110px]",
+      h2: "font-anton text-[40px] leading-[50px] lg:text-[64px] lg:leading-[77px]",
+      h3: "font-anton text-[32px] leading-[40px] lg:text-[48px] lg:leading-[58px]",
+      h4: "font-anton text-[28px] leading-[36px] lg:text-[40px] lg:leading-[50px]",
+      h5: "font-anton text-[24px] leading-[31px] lg:text-[32px] lg:leading-[40px]",
+      h6: "font-anton text-[20px] leading-[27px] lg:text-[24px] lg:leading-[31px]",
+      subtitle:
+        "font-anton text-[18px] leading-[25px] lg:text-[20px] lg:leading-[28px]",
+      "body-xl":
+        "font-inter text-[20px] leading-[30px] lg:text-[24px] lg:leading-[38px]",
+      "body-lg":
+        "font-inter text-[18px] leading-[31px] lg:text-[20px] lg:leading-[32px]",
+      "body-md":
+        "font-inter text-[16px] leading-[27px] lg:text-[18px] lg:leading-[29px]",
+      "body-sm":
+        "font-inter text-[15px] leading-[26px] lg:text-[16px] lg:leading-[27px]",
       caption: "font-inter text-[15px] leading-[24px]",
-      "button-regular": "font-inter text-[16px] leading-[27px] font-bold",
-      "button-lg": "font-inter text-[18px] leading-[29px] font-bold",
+      "button-regular": "font-inter font-bold text-[16px] lg:leading-[27px]",
+      "button-lg":
+        "font-inter font-bold text-[17px] leading-[24px] lg:text-[18px] lg:leading-[29px]",
     },
     fontWeight: {
       light: "font-light",

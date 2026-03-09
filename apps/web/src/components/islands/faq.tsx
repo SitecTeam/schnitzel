9d9f86a92f77a951ca09f811ac3a62cd20008111
Andrej Jurisic <andrejjurisic99@gmail.com>
2026-03-09 15:00:48 +0100

    fix: update styles and improve layout in About Us and FAQ components

--- apps/web/src/components/islands/faq.tsx
+++ apps/web/src/components/islands/faq.tsx
@@ -4,7 +4,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { ChevronDown } from "lucide-react";
+import { ChevronUp } from "lucide-react";
 import Typography from "../typography";
 
 const faqItems = [
@@ -37,15 +37,19 @@ const faqItems = [
 const Faq = () => {
   return (
     <section className="flex w-full justify-center px-4 lg:px-10">
-      <div className="z-20 mt-1 w-full max-w-380 bg-secondary p-4 sm:p-8 lg:-mt-10 lg:p-9 xl:-mt-10 2xl:-mt-20 2xl:p-16">
-        <div className="flex flex-col gap-8 lg:flex-row lg:gap-10">
-          <div className="basis-2/5 text-center text-white sm:text-start xl:basis-[45%]">
-            <Typography tag="h1" variant="h1" className="lg:-mt-2.5">
+      <div className="z-20 -mt-10 w-full max-w-380 bg-secondary p-6 sm:p-8 lg:p-9 2xl:-mt-20 2xl:p-16">
+        <div className="flex flex-col gap-10 lg:flex-row lg:gap-10">
+          <div className="basis-2/5 text-white xl:basis-[45%]">
+            <Typography
+              tag="h1"
+              variant="h1"
+              className="text-[72px] leading-[86px] lg:-mt-2.5"
+            >
               FAQ
             </Typography>
             <Typography
               variant="body-xl"
-              className="mt-6 w-full text-balance text-primary-foreground lg:min-w-70 xl:max-w-88"
+              className="mt-6 w-full text-pretty text-primary-foreground lg:min-w-70 xl:max-w-88"
             >
               Quick answers to the questions we get the most.
             </Typography>
@@ -55,22 +59,22 @@ const Faq = () => {
             type="single"
             collapsible
             defaultValue="faq-1"
-            className="w-full flex-1 space-y-4 lg:space-y-11 lg:justify-self-end"
+            className="w-full flex-1 space-y-7 lg:space-y-11 lg:justify-self-end"
           >
             {faqItems.map(item => (
               <AccordionItem key={item.id} value={item.id} className="border-0">
                 <AccordionTrigger className="min-h-15.25 w-full cursor-pointer items-center rounded-none bg-primary-foreground px-4 py-2 text-secondary uppercase hover:no-underline [&>svg]:hidden [&[data-state=open]>div>svg]:rotate-180">
-                  <div className="flex w-full items-start justify-between gap-4">
+                  <div className="flex w-full items-center justify-between lg:gap-4">
                     <Typography variant="h5" tag="h5">
                       {item.question}
                     </Typography>
-                    <ChevronDown className="size-8 shrink-0 text-secondary transition-transform duration-200 lg:mt-0.5 lg:size-9" />
+                    <ChevronUp className="size-9 shrink-0 text-secondary transition-transform duration-200 lg:mt-0.5" />
                   </div>
                 </AccordionTrigger>
-                <AccordionContent className="pt-4 pb-0 text-primary-foreground">
+                <AccordionContent className="pt-2 pb-0 text-primary-foreground lg:pt-4">
                   <Typography
                     variant="body-lg"
-                    className="leading-tight text-pretty"
+                    className="text-[16px] leading-[27px] text-pretty"
                   >
                     {item.answer}
                   </Typography>

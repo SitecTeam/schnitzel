b374bc6f71c5ffcdf7d5e38efbc0eabc238c69db
Andrej Jurisic <andrejjurisic99@gmail.com>
2026-03-19 09:53:50 +0100

    test

--- apps/web/src/components/islands/podbean/podbean-episode-player.tsx
+++ apps/web/src/components/islands/podbean/podbean-episode-player.tsx
@@ -13,19 +13,19 @@ const PodbeanEpisodePlayer = ({ id, title }: PodbeanEpisodePlayerProps) => {
     <section className="flex h-41.5 w-full max-w-250 flex-col justify-center lg:mt-3.5">
       <div className="bg-white p-2 sm:m-2">
         <iframe
-          title={title}
+          title="#31 James Mchaffie"
           height="150"
           width="100%"
           style={{
             border: "none",
             minWidth: "min(100%, 430px)",
-            height: "150",
+            height: "150px",
           }}
-          referrerPolicy="no-referrer"
+          scrolling="no"
           data-name="pb-iframe-player"
-          src={iframeSrc}
+          src="https://www.podbean.com/player-v2/?from=embed&i=5rjbn-1a356c3-pb&share=1&download=1&fonts=Arial&skin=1&font-color=auto&rtl=0&logo_link=episode_page&btn-skin=7&size=150"
           loading="lazy"
-        />
+        ></iframe>
       </div>
     </section>
   );

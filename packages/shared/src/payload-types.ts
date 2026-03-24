4684f9367adb0ac21db50cf6750c47f1705ac028
Andrej Jurisic <andrejjurisic99@gmail.com>
2026-03-24 12:21:34 +0100

    Refactor typography variants in episode pagination and podcast button components for consistency; update layout styles in header

--- packages/shared/src/payload-types.ts
+++ packages/shared/src/payload-types.ts
@@ -457,3 +457,7 @@ export interface PayloadMigrationsSelect<T extends boolean = true> {
 export interface Auth {
   [k: string]: unknown;
 }
+
+declare module "payload" {
+  export interface GeneratedTypes extends Config {}
+}
